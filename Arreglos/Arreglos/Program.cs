db3230df7e7a2a52e169cd73059cd73208ffb2c4
i8592 <i8592@WKSFIN259>
2019-01-10 17:41:14 +0100

    Arreglos

---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arreglo = new int[12];
+            int i;
+            string cadena;
+            for (i = 0; i < 12; i++)
+            {
+                cadena = Console.ReadLine();
+                Arreglo[i] = Convert.ToInt32(cadena);
+            }
+            for (i = 0; i < 12; i++)
+            {
+                Console.WriteLine("{0}", Arreglo[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
