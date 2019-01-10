dff14bf4a1b8bc3a1abdc11490fc420455d20016
i8592 <i8592@WKSFIN259>
2019-01-10 12:33:37 +0100

    clases abstractas

---
+++ ClasesAbstractas/ClasesAbstractas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClientesBase cliente = new clsClientesBase(0, "Marcos Hernandez",
+                "001", "HEHM", 1,"Marcos Henandez");
+            Console.WriteLine(cliente.Clave + " " + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+        
+        }
+    }
+}
