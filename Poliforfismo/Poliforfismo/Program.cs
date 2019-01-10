388332cf860afa74e479e0ab67d4e6eed729e52b
i8592 <i8592@WKSFIN259>
2019-01-10 13:31:14 +0100

    Polimorfismo

---
+++ Poliforfismo/Poliforfismo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliforfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClientesConContacto cliente = new clsClientesConContacto(0, "Marcos Hernandez",
+                                                                        "001",
+                                                                        "HEHM",
+                                                                        1, "Marcos Hernandez",
+                                                                        "666666666",
+                                                                        "666333333",
+                                                                        "MH@yahoo.es",
+                                                                        "Av Caidos",
+                                                                        "124", "1","Chiapas",
+                                                                        "Chiapas", "a","29096");
+            clsImpresion Impresion;
+            Impresion = new clsImpresion();
+            Impresion.ImprimeCliente(cliente);
+        }
+    }
+}
---
+++ Poliforfismo/Poliforfismo/clsImpresion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliforfismo
+{
+    public class clsImpresion
+    {
+        public void ImprimeCliente(clsAbsClientes cliente)
+        {
+            Console.WriteLine(cliente.Clave + " " + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+        }
+    }
+}
