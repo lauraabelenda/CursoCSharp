5e0a7c4a703aea1c6345005caabb4c233304952e
i8592 <i8592@WKSFIN259>
2019-01-11 08:47:52 +0100

    Colecciones ArrayList

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsMostrarArrayList Muestra = new clsMostrarArrayList();
+            Muestra.CapturarDatos();
+            Muestra.ImprimeDatos();
+
+        }
+    }
+}
---
+++ ArrayList/ArrayList/clsMostrarArrayList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    public class clsMostrarArrayList
+    {
+        ArrayList Lista;
+        
+        public clsMostrarArrayList()
+        {
+            Lista = new ArrayList();
+        }
+
+        public void CapturarDatos()
+        {
+            string cadena;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Dame el dato {0}: ",i + 1);
+                cadena = Console.ReadLine();
+                Lista.Add(cadena);
+            }
+        }
+
+        public void ImprimeDatos()
+        {
+            foreach (var item in Lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
+
