4b075f3d747dff8b479c188fd026cd388bff5f36
i8592 <i8592@WKSFIN259>
2019-01-11 09:18:10 +0100

    HashTable

---
+++ MuestraHashTable/MuestraHashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuestraHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //clsMostrarArrayList arl;
+            //arl = new clsMostrarArrayList();
+            //arl.CapturarDatos();
+            //arl.ImprimeDatos();
+
+            clsMuestraHashTable MuestraH;
+            MuestraH = new clsMuestraHashTable();
+            MuestraH.AgregarDatos();
+            MuestraH.ImprimirDatos();
+            MuestraH.EliminarElemento("C001");
+            MuestraH.ImprimirDatos();
+        }
+    }
+}
---
+++ MuestraHashTable/MuestraHashTable/clsMuestraHashTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuestraHashTable
+{
+    public class clsMuestraHashTable
+    {
+        Hashtable Tabla; 
+        public clsMuestraHashTable()
+        {
+            Tabla = new Hashtable();
+
+        }
+
+        public void AgregarDatos()
+        {
+            Tabla.Add("C001", "Producto 1");
+            Tabla.Add("C002", "Producto 2");
+            Tabla.Add("C003", "Producto 3");
+            Tabla.Add("C004", "Producto 4");
+        }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Existen {0} elementos, dame la clave del producto", Tabla.Count);
+            string cadena = Console.ReadLine();
+            if (Tabla[cadena] == null)
+                Console.WriteLine("El elemento no existe");
+            else
+            {
+                Console.WriteLine("El elemento encontrado es: ");
+                Console.WriteLine(Tabla[cadena]);
+            }
+            Console.ReadKey();
+        }
+
+        public void EliminarElemento(string key)
+        {
+            Tabla.Remove(key);
+        }
+    }
+}
