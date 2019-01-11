1f4b1c899fb5e6a843454aa79b135abaac32d4b7
i8592 <i8592@WKSFIN259>
2019-01-11 08:26:38 +0100

    Matrices Multidimensionales

---
+++ ArreglosMultidimensionales/ArreglosMultidimensionales/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosMultidimensionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsMatrices Matriz;
+            Matriz = new clsMatrices();
+            Matriz.InicializaMatriz();
+            Matriz.ImprimeMatriz();
+        }
+    }
+}
---
+++ ArreglosMultidimensionales/ArreglosMultidimensionales/clsMatrices.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosMultidimensionales
+{
+    public class clsMatrices
+    {
+        private int[,] Matriz;
+
+        public clsMatrices()
+        {
+            Matriz = new int[4, 2];
+        }
+
+        public void InicializaMatriz()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Matriz[i, j] = 1;
+                }
+            }
+        }
+
+        public void ImprimeMatriz()
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("{0} ", Matriz[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
