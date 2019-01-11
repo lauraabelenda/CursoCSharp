1cc3e1601e817f7b4e0547eba99c796cf40421f8
i8592 <i8592@WKSFIN259>
2019-01-11 09:44:07 +0100

    Genericos

---
+++ Genericos/Genericos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion = "";
+            List<clsClientesBase> Clientes;
+            Clientes = new List<clsClientesBase>();
+
+            while (opcion != "s")
+            {
+                Console.WriteLine("Opciones: ");
+                Console.WriteLine("a. Agregar a lalista");
+                Console.WriteLine("b. Quitar de la lista");
+                Console.WriteLine("c. Imprimir lista");
+                Console.WriteLine();
+                Console.WriteLine("s. Salir");
+                opcion = Console.ReadLine();
+                if(opcion == "a")
+                {
+                    string cadena = Console.ReadLine();
+                    clsClientesBase cli = new clsClientesBase();
+                    cli.Id = Clientes.Count + 1;
+                    cli.Nombre = cadena;
+                    Clientes.Add(cli);
+                }
+                else if(opcion == "b")
+                {
+                    string cadena = Console.ReadLine();
+                    Clientes.RemoveAt(Convert.ToInt32(cadena)); 
+                }
+                else if(opcion == "c")
+                {
+                    foreach (clsClientesBase item in Clientes)
+                    {
+                        Console.WriteLine("Los datos del cliente son: ");
+                        Console.WriteLine(item.Id.ToString() + " " + item.Nombre);
+                    }
+                }
+
+
+            }
+        }
+    }
+}
